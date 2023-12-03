94c45450a96d69215e033d070cd9334f578e65db
cow11023 <cow11023@163.com>
2023-12-04 02:26:37 +0800

    增加创建任务的按钮及相关逻辑，并写写CloudKit数据库

--- TaskClock/TasksMenuView.swift
+++ TaskClock/TasksMenuView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CloudKit
 
-//定义任务列表的数据结构
+// 定义任务列表的数据结构
 struct Task: Identifiable {
     var id: String
     var name: String {
@@ -17,11 +17,11 @@ struct Task: Identifiable {
             limitChineseCharacterCount()
         }
     }
-    var SceduleID: Int64
-    var isActivated: Bool //任务是否启用
+    var isActivated: Bool // 任务是否启用
     var createtime: TimeZone
     var updatetime: TimeZone
 
+    // 检查中文字符数量是否超过20的私有方法
     private func limitChineseCharacterCount() {
         let chineseCharacterCount = name.countChineseCharacters()
         if chineseCharacterCount > 20 {
@@ -30,6 +30,7 @@ struct Task: Identifiable {
         }
     }
 
+    // 显示错误提示的私有方法
     private func showErrorAlert() {
         // 显示错误提示
         // 这里可以使用一个状态变量来控制 Alert 的显示
@@ -40,8 +41,8 @@ struct Task: Identifiable {
     }
 }
 
+// 扩展 String 类型以计算中文字符数量
 extension String {
-    // 计算中文字符数量的扩展
     func countChineseCharacters() -> Int {
         var count = 0
         for scalar in String(self).unicodeScalars {
@@ -53,18 +54,19 @@ extension String {
     }
 }
 
+// 扩展 UnicodeScalar 类型以判断字符是否为中文
 extension UnicodeScalar {
-    // 判断字符是否是中文的扩展
     var isChinese: Bool {
-        //return ("一" <= self && self <= "龥")
         return (0x4E00 <= value && value <= 0x9FFF)
     }
 }
 
-
 struct TasksMenuView: View {
-    @State private var tasks: [Task] = []
     
+    @State private var tasks: [Task] = []
+    @State private var newTaskName: String = ""
+    @State private var isShowingErrorAlert = false
+
     init() {
         loadTasks()
     }
@@ -77,7 +79,7 @@ struct TasksMenuView: View {
                 .scaleEffect(x: -1)
 
             ScrollView {
-                VStack(alignment: .leading, spacing: 10) { // 将 alignment 设置为 .leading
+                VStack(alignment: .leading, spacing: 10) {
                     GeometryReader { geo in
                         HStack {
                             Text("任务列表")
@@ -89,6 +91,7 @@ struct TasksMenuView: View {
                         .frame(maxWidth: .infinity)
                         .frame(height: 100)
 
+                        // 列表显示任务
                         List(tasks) { task in
                             Text(task.name)
                                 .foregroundColor(.black)
@@ -99,15 +102,87 @@ struct TasksMenuView: View {
                         }
                     }
                     .padding()
+
+                    // Spacer() 添加垂直空间
+                    Spacer()
+
+                    // Spacer() 添加额外的垂直空间，这里可以调整高度
+                    Spacer().frame(height: 120)
+
+                    // 添加用于创建任务的按钮和文本字段
+                    HStack {
+                        TextField("新任务", text: $newTaskName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal)
+
+                        Button(action: {
+                            // 点击按钮时调用添加任务的逻辑
+                            addTask()
+                        }) {
+                            Text("添加任务")
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).foregroundColor(.yellow))
+                                .foregroundColor(.black)
+                        }
+                    }
+                    .padding()
                 }
+            }
+            .alert(isPresented: $isShowingErrorAlert) {
+                Alert(title: Text("超过20个中文字符，请重新输入"))
+            }
+        }
+    }
+
+    private func addTask() {
+        let chineseCharacterCount = newTaskName.countChineseCharacters()
+        if chineseCharacterCount > 20 {
+            isShowingErrorAlert = true
+        } else {
+            let newTask = Task(
+                id: UUID().uuidString,
+                name: newTaskName,
+                isActivated: false,
+                createtime: TimeZone.current,
+                updatetime: TimeZone.current
+            )
+
+            tasks.append(newTask)
+            newTaskName = ""
+
+            // 将新任务数据同步到 CloudKit 中
+            TaskToCloudKit(task: newTask)
+        }
+    }
+
+    private func TaskToCloudKit(task: Task) {
+        let container = CKContainer(identifier: "iCloud.TaskClock")
+        let db = container.privateCloudDatabase
+
+        // 创建 CKRecord 对象
+        let record = CKRecord(recordType: "Task")
+        record["id"] = task.id
+        record["name"] = task.name
+        record["isActivated"] = task.isActivated
+        if let createtimeData = try? NSKeyedArchiver.archivedData(withRootObject: task.createtime, requiringSecureCoding: false) {
+            record["createtime"] = createtimeData as CKRecordValue
+        }
+        if let updatetimeData = try? NSKeyedArchiver.archivedData(withRootObject: task.updatetime, requiringSecureCoding: false) {
+            record["updatetime"] = updatetimeData as CKRecordValue
+        }
+
+        // 将 CKRecord 保存到 CloudKit 数据库
+        db.save(record) { (record, error) in
+            if let error = error {
+                print("保存任务到 CloudKit 出错: \(error.localizedDescription)")
+            } else {
+                print("任务保存到 CloudKit 成功")
             }
         }
     }
 
 
-
     private func loadTasks() {
-        // 使用 CloudKit 查询任务列表数据
         let container = CKContainer(identifier: "iCloud.TaskClock")
         let db = container.privateCloudDatabase
         let query = CKQuery(recordType: "Task", predicate: NSPredicate(value: true))
@@ -118,20 +193,20 @@ struct TasksMenuView: View {
                 DispatchQueue.main.async {
                     self.tasks = records.map { record in
                         Task(
-                            id: record.recordID.recordName,
+                            id: record["id"] as? String ?? "",
                             name: record["name"] as? String ?? "",
-                            SceduleID: record["scheduleID"] as? Int64 ?? 0,
                             isActivated: record["isActivated"] as? Bool ?? false,
                             createtime: record["createtime"] as? TimeZone ?? TimeZone.current,
                             updatetime: record["updatetime"] as? TimeZone ?? TimeZone.current
                         )
                     }
                 }
-                print("开始打印数据",self.tasks)
+                print("开始打印数据", self.tasks)
             }
         }
     }
 }
+
 
 struct TasksMenuView_Previews: PreviewProvider {
     static var previews: some View {
